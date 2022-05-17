e728c552f0b3a3f838aacdfc966b49cebddae0af
daryl-san <99176560+daryl-san@users.noreply.github.com>
2022-05-17 22:40:24 +1000

    initial display

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import itemlist from './itemlist';
 import React, { Component } from 'react';
 
-class App() extends Component{
+class App extends Component {
 
   constructor() {
     super();
@@ -12,13 +12,24 @@ class App() extends Component{
     }
   }
 
-  return !itemlist.length ?
-    <h1>Loading...</h1> :
-    (
-      <div>
-        <h1>My item List</h1>
-      </div>
-    );
+
+  render() {
+    // return (<h1>Loading...</h1>)
+    return !itemlist.length ?
+      <h1>Loading...</h1> :
+      (
+        <div>
+          <h1>My item List</h1>
+          <ol>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 4</li>
+            <li>item 5</li>
+          </ol>
+        </div>
+      );
+  };
 }
 
 export default App;
