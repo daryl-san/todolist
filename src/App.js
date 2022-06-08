a50e74f3a6b8dd1ac3cef1adbbe32ec2da156815
daryl-san <99176560+daryl-san@users.noreply.github.com>
2022-06-08 20:23:36 +1000

    quick code clean up to remove warning upon compile (made sure filter is returning for else case)

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ class App extends Component {
     var filteredList = this.state.Itemlist.filter(item => {
       if (parseInt(item.number) !== parseInt(event.target.value)) {
         return item;
+      } else {
+        return null;
       }
     })
 
